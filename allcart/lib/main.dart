16968aa97fd3ce1201d353d5bc5e743603e6406b
Joshua Joseph <joshua841015@gmail.com>
2021-05-24 13:38:01 +0530

    Delete allcart directory

--- allcart/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:allcart/pages/Loading.dart';
-
-void main() => runApp(MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Loading(),
-      },
-    ));
